14386b0357c0ff75a10e7c457ba301ecb6828dce
Lorenzo Quiambao <lorenzotquiambao@gmail.com>
2022-12-07 09:44:29 -0500

    removed unused routes

--- routes/schedule-routes.js
+++ routes/schedule-routes.js
@@ -4,26 +4,16 @@ const router = express.Router();
 
 const {
   getSchedule,
-  getGoogleEvents,
   createEvent,
-  automateSchedule,
-} = require("../controllers/schedule-controller.js");
+} = require("../controllers/sched-controller.js");
 // import authenticateUser from "../middleware/auth.js";
 router.route("/").get(
   // apiLimiter,
   getSchedule
 );
-router.route("/event/google").get(
-  // apiLimiter,
-  getGoogleEvents
-);
 router.route("/event").post(
   // apiLimiter,
   createEvent
-);
-router.route("/automate").post(
-  // apiLimiter,
-  automateSchedule
 );
 
 module.exports = router;
